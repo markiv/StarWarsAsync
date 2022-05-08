b111fe67ede1e264970e01b1a00593455fe4ec9c
Vikram Kriplaney <markiv@gmail.com>
2022-05-08 22:49:16 +0200

    Initial Commit

---
+++ StarWarsAsync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsAsync
+//
+//  Created by Vikram Kriplaney on 08.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsAsync/StarWarsAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsAsyncApp.swift
+//  StarWarsAsync
+//
+//  Created by Vikram Kriplaney on 08.05.22.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
